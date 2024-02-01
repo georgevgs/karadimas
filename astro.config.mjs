b33f6e8fe569a58658ae6004e0268142744fa0f1
George Vagdas <dev@vagdas.eu>
2024-02-01 13:02:34 +0200

    Added sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,23 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
-import compress from 'astro-compress'
-import icon from "astro-icon"
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ikaradimas.gr",
   compressHTML: true,
-  integrations: [mdx(), icon(), tailwind({
-    applyBaseStyles: false,
-  }), compress()],
-})
+  integrations: [
+    sitemap(),
+    mdx(),
+    icon(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    compress(),
+    robotsTxt(),
+  ],
+});
