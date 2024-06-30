5564cb2ed1b1ae072aac4f5c9a43ee4bc352a415
George Vagdas <dev@vagdas.eu>
2024-06-30 19:28:30 +0300

    Added Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,29 @@
-import { defineConfig } from "astro/config";
+import {defineConfig} from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import icon from "astro-icon";
+import vercel from "@astrojs/vercel/static";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://ikaradimas.gr",
-  compressHTML: true,
-  integrations: [
-    sitemap(),
-    mdx(),
-    icon(),
-    tailwind({
-      applyBaseStyles: false,
+    site: "https://ikaradimas.gr",
+    compressHTML: true,
+    output: "static",
+    adapter: vercel({
+        webAnalytics: {
+            enabled: true,
+        },
     }),
-    compress(),
-    robotsTxt(),
-  ],
+    integrations: [
+        sitemap(),
+        mdx(),
+        icon(),
+        tailwind({
+            applyBaseStyles: false
+        }),
+        compress(),
+        robotsTxt()
+    ],
 });
